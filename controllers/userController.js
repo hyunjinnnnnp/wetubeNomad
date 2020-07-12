79c67aa137ed4d26fcec48e59dafbe99d9f4947d
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2020-07-12 19:22:43 +0900

    fake db

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,27 @@
-export const join = (req, res) => res.send("join");
-export const login = (req, res) => res.send("login");
-export const logout = (req, res) => res.send("logout");
-export const users = (req, res) => res.send("users");
-export const userDetail = (req, res) => res.send("userDetail");
-export const editProfile = (req, res) => res.send("editProfile");
-export const changePassword = (req, res) => res.send("changePassword");
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+    res.render("join", {pageTitle: 'Join'});
+
+}
+export const postJoin = (req, res) => {
+    const{
+        body: { name, email, password, password2}
+    } = req;
+    if(password !== password2){
+        res.status(400);
+        res.render("join", {pageTitle: 'Join'});
+    }else{
+        // to do : register user
+        // to do : log user in
+        res.redirect(routes.home)
+    }
+}
+
+
+export const login = (req, res) => res.render("login", {pageTitle: 'Login'});
+export const logout = (req, res) => res.render("logout", {pageTitle: 'Logout'});
+export const users = (req, res) => res.render("users", {pageTitle: 'Users'});
+export const userDetail = (req, res) => res.render("userDetail", {pageTitle: 'User Detail'});
+export const editProfile = (req, res) => res.render("editProfile", {pageTitle: 'Edit Profile'});
+export const changePassword = (req, res) => res.render("changePassword", {pageTitle: 'Change Password'});
---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+{
+    id: 23124,
+    title: 'Video awesome',
+    description: 'This is something I love',
+    views: 23,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+        id: 2414,
+        name: "Nicolas",
+        email: 'nico@las.com'
+    }
+},
+{
+    id: 1244,
+    title: 'Video funny',
+    description: 'This is something I love',
+    views: 23,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+        id: 2414,
+        name: "Nicolas",
+        email: 'nico@las.com'
+    }
+},
+{
+    id: 224,
+    title: 'Video super',
+    description: 'This is something I love',
+    views: 23,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+        id: 2414,
+        name: "Nicolas",
+        email: 'nico@las.com'
+    }
+}
+]
